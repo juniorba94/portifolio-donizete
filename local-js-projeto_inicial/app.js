1aeb9f4fcffcbeb4feb0c3ba9d5837ebef06f947
DONIZETE BATISTA JUNIOR <donizete.b.batista@bradesco.com.br>
2025-05-13 16:48:00 -0300

    Loop Parte 1 - update

--- local-js-projeto_inicial/app.js
+++ local-js-projeto_inicial/app.js
@@ -2,13 +2,10 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 
 let chute = prompt('Escolha um número entre 1 e 10');
-console.log('Valor do chute:', chute);
-// se chute for igual ao número secreto
 
-console.log('Resultado da comparação:', chute == numeroSecreto);
+// se chute for igual ao número secreto
 if(chute == numeroSecreto){
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-     console.log('Valor do número secreto:', numeroSecreto);
 }
 else{
     alert('Você errou :(');
