553b897bd69cb0a12141b975f7dcdb12b097ac08
DONIZETE BATISTA JUNIOR <donizete.b.batista@bradesco.com.br>
2025-05-13 16:46:34 -0300

    Loop Parte 1

---
+++ desafio-2/app.js
@@ -0,0 +1,31 @@
+//let diaEscolhido = prompt('Escolha um dia da semana: ');
+//
+//if(diaEscolhido == "Sábado"){
+//    alert("Bom fim de semana");
+//}else if(diaEscolhido == "Domingo"){
+//    alert("Bom fim de semana");
+//}else{
+//    alert("Boa semana");
+//}
+
+//numero = prompt("Informe o número:");
+//    if(numero < 0){
+//        alert("O número é negativo!");
+//    }else{
+//        alert("O número é positivo!");
+//}
+//pontuacao = prompt("Informe a pontuação:");
+//
+//if(pontuacao >= 100){
+//    alert("Parabéns, você venceu!");
+//} else{
+//    alert("Tente novamente para ganhar.");
+//}
+
+//saldoConta = 500;
+//alert(`Seu saldo é ${saldoConta}.`)
+
+
+nome = prompt('Informe seu nome:');
+alert(`Boas vindas ${nome}.`)
+
--- local-js-projeto_inicial/app.js
+++ local-js-projeto_inicial/app.js
@@ -1,11 +1,14 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 9;
 
+let chute = prompt('Escolha um número entre 1 e 10');
+console.log('Valor do chute:', chute);
 // se chute for igual ao número secreto
+
+console.log('Resultado da comparação:', chute == numeroSecreto);
 if(chute == numeroSecreto){
-    alert('Isso ai! Você descobriu o número secreto(5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+     console.log('Valor do número secreto:', numeroSecreto);
 }
 else{
     alert('Você errou :(');
