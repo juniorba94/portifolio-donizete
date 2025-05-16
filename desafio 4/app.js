e35bb2286d0e31d18bc2d3d2d8e2962e80c4616f
Donizete Batista Junior <juniorba94@outlook.com>
2025-05-15 22:20:35 -0300

    Desafio 4

---
+++ desafio 4/app.js
@@ -0,0 +1,93 @@
+// Ex 1 - console.log('Boas vindas');
+
+// Ex 2
+
+//let nome = 'Doni';
+//console.log(`Olá ${nome}!`);
+
+// Ex 3 
+
+// let nome = 'Doni';
+// alert(`Olá ${nome}!`);
+
+// Ex 4
+
+// let linguagem = prompt('Qual a linguagem de programação que você mais gosta?');
+// console.log(linguagem);
+
+// Ex 5 - Soma
+
+// let valor1 = 2;
+// let valor2 = 3;
+// 
+// let resultado  = valor1 + valor2;
+// 
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+// Ex 6 - Subtração
+
+// let valor1 = 8;
+// let valor2 = 3;
+
+// let resultado  = valor1 - valor2;
+
+// console.log(`A diferença de ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+// Ex 7 - Menor o Maior de Idade
+
+// let idade = prompt('Qual é sua idade?:');
+
+// if(idade < 18){
+//     console.log('Você é menor de idade');
+// } else{
+//         console.log('Você é maior de idade');
+// }
+
+//  EX 8 - positivo, negativo e zero
+
+// let numero = prompt('Digite um número:');
+
+// if(numero == 0){
+//     console.log('Número é zero.');
+// } else{
+//     if(numero < 0){
+//         console.log('Número é Negativo.');
+//     }else{
+//         console.log('Número é Positivo.');
+//     }
+        
+// }
+
+// Ex 9 - Imprima Números.
+
+// let numero = 1;
+
+// while(numero <= 10){
+//   console.log(numero);
+//   numero++;
+// }
+
+//  Ex 10 - Notas
+
+// let nota = 5;
+// if(nota >= 7){
+//     console.log("Aprovado");
+// }else{
+//      console.log("Reprovado");
+// }
+
+// Ex 11 - N aleatório.
+
+// let numeroAleatorio = Math.random();
+// console.log(numeroAleatorio);
+
+// Ex 12 - Aleatorio 1 a 10
+
+// let numeroAleatorio = parseInt(Math.random()* 10 + 1);
+// console.log(numeroAleatorio);
+
+// Ex 13 - Aleatorio 1 a 100
+
+let numeroAleatorio = parseInt(Math.random()* 100 + 1);
+console.log(numeroAleatorio);
+
