c66bc295c8ec4ee1ec17990914b9dc2650abb15e
Donizete Batista Junior <juniorba94@outlook.com>
2025-06-10 19:41:44 -0300

    App

--- Curso-Testes-Unitários-Jest/organo-projeto-base/src/app/services/livro.service.spec.ts
+++ Curso-Testes-Unitários-Jest/organo-projeto-base/src/app/services/livro.service.spec.ts
@@ -1,26 +1,36 @@
 import { Livro } from "../componentes/livro/livro";
+import { livros } from "../mock-livros";
 import { LivroService } from "./livro.service"
 
 describe('LivroService', () => {
-    let service : LivroService;
+    let service: LivroService;
 
-    it('deveria ser criado', () =>{
+    beforeEach(() => {
         service = new LivroService();
+    })
+
+    it('deveria ser criado', () => {
         expect(service).toBeTruthy();
     })
 
-   //Código omitido
+    it('deveria adicionar um novo livro', () => {
+        const novoLivro: Livro = {
+            titulo: 'Novo Livro',
+            autoria: 'Autor Desconhecido',
+            imagem: 'http://example.com/cover.jpg',
+            genero: { id: 'romance', value: 'Romance' },
+            dataLeitura: '2024-04-19',
+            classificacao: 5
+        };
 
-it('deveria adicionar um novo livro', () => {
-    service = new LivroService();
-    const novoLivro: Livro = {
-        titulo: 'Novo Livro',
-        autoria: 'Autor Desconhecido',
-        imagem: 'http://example.com/cover.jpg',
-        genero: {id: 'romance', value: 'Romance'},
-        dataLeitura: '2024-04-19',
-        classificacao: 5 
-        
-    }});
+        service.adicionarLivro(novoLivro);
+        const livrosporGenero = service.obterLivrosPorGenero('romance')
+        expect(livrosporGenero).toContain(novoLivro);
+    })
+
+    it('deveria recuperar corretamente os livros por gênero', () => {
+        const livrosporGenero = service.obterLivrosPorGenero('romance')
+        const livrosEsperados = livros.filter(livro => livro.genero.id === 'romance');
+        expect(livrosporGenero).toEqual(livrosEsperados);
+    })
 });
-
