b699c4d3ad69a251fdf972b4f7dc5b497878168c
Donizete Batista Junior <donizetej@ciandt.com>
2025-07-18 23:00:58 -0300

    Azul

---
+++ Curso-Logica-Programacao/js-curso-2-aula1/desafio 3/app.js
@@ -0,0 +1,15 @@
+/* Exercicio 1 -   Lista Generica*/
+
+let listaGenerica = [];
+
+/* Exercicio 2 -   Lista de Programacao*/
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin' , 'Python'];
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+nomes = ['Java', 'Ruby', 'GoLang'];
+
+console.log(nomes[0]);
+console.log(nomes[1]);
+console.log(nomes[2]);
